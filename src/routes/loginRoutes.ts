9fc9bd99be88a467b261ce1c48f9885dc6bbb807
박도희 <dohee.pa@gmail.com>
2021-04-22 22:28:11 +0900

    219. Wiring Up Session

--- src/routes/loginRoutes.ts
+++ src/routes/loginRoutes.ts
@@ -25,10 +25,13 @@ router.get('/login', (req: Request, res: Response) => {
 router.post('/login', (req: RequestWithBody, res: Response) => {
   const { email, password } = req.body;
 
-  if (email) {
-    res.send(email.toUpperCase());
+  req.session
+
+  if (email && password && email === 'hi@hi.com' && password === 'password') {
+    req.session = { loggedIn: true };
+    res.redirect('/');
   } else {
-    res.send('You must provide an email');
+    res.send('Invalid email or password');
   }
 });
 
