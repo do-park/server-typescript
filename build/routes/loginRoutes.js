d358b498e1238f93abc631414318380dedf49056
박도희 <dohee.pa@gmail.com>
2021-04-22 22:32:25 +0900

    220. Checking Login Status

--- build/routes/loginRoutes.js
+++ build/routes/loginRoutes.js
@@ -18,3 +18,12 @@ router.post('/login', function (req, res) {
         res.send('Invalid email or password');
     }
 });
+router.get('/', function (req, res) {
+    var _a;
+    if ((_a = req === null || req === void 0 ? void 0 : req.session) === null || _a === void 0 ? void 0 : _a.loggedIn) {
+        res.send("\n      <div>\n        <div>You are logged in</div>\n        <a href=\"/logout\">Logout</a>\n      </div>\n    ");
+    }
+    else {
+        res.send("\n      <div>\n        <div>You are not logged in</div>\n        <a href=\"/login\">Login</a>\n      </div>\n    ");
+    }
+});
