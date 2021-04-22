a4bdd4673dfef3c2666254786898392cc7083525
박도희 <dohee.pa@gmail.com>
2021-04-22 22:34:04 +0900

    221. Logging out

--- src/routes/loginRoutes.ts
+++ src/routes/loginRoutes.ts
@@ -51,6 +51,11 @@ router.get('/', (req: Request, res: Response) => {
       </div>
     `);
   }
-})
+});
+
+router.get('/logout', (req: Request, res: Response) => {
+  req.session = undefined;
+  res.redirect('/');
+});
 
 export { router };
