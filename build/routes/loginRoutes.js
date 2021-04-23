49578505d7773dd44a66c0cbc64f4a031e579985
박도희 <dohee.pa@gmail.com>
2021-04-23 23:18:59 +0900

    222. Protecting Routes

--- build/routes/loginRoutes.js
+++ build/routes/loginRoutes.js
@@ -2,6 +2,15 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.router = void 0;
 var express_1 = require("express");
+function requireAuth(req, res, next) {
+    var _a;
+    if ((_a = req === null || req === void 0 ? void 0 : req.session) === null || _a === void 0 ? void 0 : _a.loggedIn) {
+        next();
+        return;
+    }
+    res.status(403);
+    res.send('Not permitted');
+}
 var router = express_1.Router();
 exports.router = router;
 router.get('/login', function (req, res) {
@@ -30,4 +39,7 @@ router.get('/', function (req, res) {
 router.get('/logout', function (req, res) {
     req.session = undefined;
     res.redirect('/');
+});
+router.get('/protected', requireAuth, function (req, res) {
+    res.send('Welcome to protected route, logged in user.');
 });
